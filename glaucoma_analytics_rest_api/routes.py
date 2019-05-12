ddbbe85d07e1255321f48b0c209f729c2e7bc7bb
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-05-13 00:03:18 +1000

    [analytics.py] Expose start and end datetimes; typed major functions; fixed typos; [routes.py] Input start and end datetimes from route; [__init__.py] Bump version

--- glaucoma_analytics_rest_api/routes.py
+++ glaucoma_analytics_rest_api/routes.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from bottle import response
+from bottle import response, request
 
 from glaucoma_analytics_rest_api import rest_api, __version__
-from glaucoma_analytics_rest_api.analytics import run
+from glaucoma_analytics_rest_api.analytics import run, sydney
 from glaucoma_analytics_rest_api.utils import auth_needed
 
 
@@ -14,7 +14,15 @@ def enable_cors():
 
 @rest_api.route('/api/py/analytics', apply=[auth_needed])
 def analytics():
-    return run()
+    if request.params.startDatetime and request.params.endDatetime:
+        event_start = datetime.strptime(request.params.startDatetime, "%Y-%m-%dT%H:%M:%S%z")
+        event_end = datetime.strptime(request.params.endDatetime, "%Y-%m-%dT%H:%M:%S%z")
+    else:
+        # Limit selection to 8AM Monday until 2:30PM Monday
+        # (which corresponds with the OPSM event start & end time)
+        event_start = datetime(year=2019, month=3, day=11, hour=8, tzinfo=sydney)
+        event_end = event_start + timedelta(hours=6, minutes=60)
+    return run(event_start, event_end)
 
 
 @rest_api.route('/api')
